f24e9702998bda12a04c07666aefbcb6a1218265
ankitt1992 <ankit.tiwari@neosofttech.com>
2017-01-31 13:32:23 +0530

    subcategory nil class error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
   def index
-   @banners = Banner.all
-   # @category = nil
-   # binding.pry
-   # @categories = Category.find(:all, :conditions => {:parent_id => nil } )
-   @categories = Category.all.where(parent_id: nil)
-   @category = Category.first
-   @subcategory = @category.subcategories.last
-   @products = @subcategory.products.limit(8)
-   @cart_item = CartItem.new
+    @banners = Banner.all
+    # @category = nil
+    # binding.pry
+    # @categories = Category.find(:all, :conditions => {:parent_id => nil } )
+    @categories = Category.all.where(parent_id: nil)
+    @category = Category.first
+    if @category.present?
+      @subcategory = @category.subcategories.last
+      @products = @subcategory.products.limit(8)
+    end
+
+    @cart_item = CartItem.new
 
     if user_signed_in?
       @cart_items = current_user.cart_items.all
